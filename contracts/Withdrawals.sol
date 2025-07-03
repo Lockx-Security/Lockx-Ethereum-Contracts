97d4dadc3eb07890a0641aec7d11333951a82066
Richard Chung <richardjaey@g.ucla.edu>
2025-07-02 20:04:52 -0700

    refactor: simplify BUSL-1.1 headers to minimal compliant format
    - Remove verbose license text, keep only required parameters
    - 3-line format: SPDX identifier + Change Date + Change License
    - Remove MariaDB link (not required in source code)
    - Remove conflicting custom copyright notices
    - Maintains legal compliance while keeping headers clean

--- contracts/Withdrawals.sol
+++ contracts/Withdrawals.sol
@@ -1,20 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
-/*
- * Licensed under the Business Source License 1.1 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * https://mariadb.com/bsl11/
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Change Date: January 1, 2029
- * Change License: MIT
- */
+// Change Date: January 1, 2029
+// Change License: MIT
 pragma solidity ^0.8.30;
 
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
