a51a42f8542d0b77f063b6d5ac827e4823b32c68
Richard Chung <richardjaey@g.ucla.edu>
2025-10-01 22:49:10 -0700

    Cleaned repo
    Removed tests, mocks, deprecated files

--- test/mocks/index.ts
+++
@@ -1,3 +0,0 @@
-export { MockFeeOnTransferToken } from '../../typechain-types/contracts/mocks/MockFeeOnTransferToken';
-export { MockERC20 } from '../../typechain-types/contracts/mocks/MockERC20';
-export { MockERC721 } from '../../typechain-types/contracts/mocks/MockERC721'; 
